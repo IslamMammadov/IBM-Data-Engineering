7bedf08049942cd39c6d8352b387cb0f8e09d77e
Islam <60185596+IslamMammadov@users.noreply.github.com>
2025-09-10 01:43:00 +0000

    Added linux files

---
+++ Linux Commands and Shell Scripting/install_text_editors.sh
@@ -0,0 +1,17 @@
+#Updating your Linux sytem's package list index
+sudo apt update
+
+#Upgrading nano
+sudo apt upgrade nano
+
+#Installing Vim
+sudo apt install vim
+
+#Navigating to the project directory
+cd 'Linux Commands and Shell Scripting'
+
+#Creating and editing a text file with nano
+nano hello_world.txt
+
+#Verifying your new text file
+cat hello_world.txt
