310472526a769e6202571c486ee3aa2783dc9a0d
Islam <60185596+IslamMammadov@users.noreply.github.com>
2025-09-10 02:31:04 +0000

    Added practise 1

---
+++ Linux Commands and Shell Scripting/linux_comm.sh
@@ -0,0 +1,46 @@
+#Getting Information
+
+whoami
+id -u -n
+uname -s -r  #-v
+ps -e
+top -n 3
+df -h ~
+man -k .
+date "+%j day of %Y!"
+
+#Working with files
+cp
+mv
+rm
+touch
+chmod
+wc
+grep
+
+#Navigating and working with file directories
+ls
+find
+pwd
+mkdir
+cd
+rmdir
+
+#Printing file and string contents
+cat
+more
+head
+tail
+echo
+
+#Compression and archiving
+zip
+tar
+unzip
+
+#Networking
+hostname
+ping
+ifconfig
+curl
+wget
---
+++ Linux Commands and Shell Scripting/practise_info_comm.sh
@@ -0,0 +1,15 @@
+#Get basic information about the operating system.
+uname -v
+
+#View all running processes on the system.
+ps -e
+
+# Get the table of processes and sort by memory usage.
+# sort by memory usage by pressing Shift + m.
+top
+
+#Display the current time.
+date "+%T"
+
+# Using one command, display the messages "Hello!" and "Goodbye!" separated by a new line.
+echo -e "Hello! \nGoodbye!"
